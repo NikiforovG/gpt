6ab07285e1ccab5af3e19b3580dacf8c5a12a2d0
gleb <nikiforovg1996@gmail.com>
2024-03-28 15:35:33 +0100

    Update training scripts and add model save/load functions

--- main/src/utils.py
+++ main/src/utils.py
@@ -1,7 +1,13 @@
+import glob
+import os
+from dataclasses import dataclass
+from typing import Any
+
 import requests
 import torch
 
 from .data import Data
+from .gpt import GPTConfig, GPTModel
 
 
 def get_tinyshakespeare_dataset() -> str:
@@ -13,7 +19,7 @@ def get_tinyshakespeare_dataset() -> str:
 
 @torch.no_grad()
 def estimate_loss(
-    eval_iters: int, model: torch.nn.Module, data: Data, block_size: int, batch_size: int
+    eval_iters: int, model: torch.nn.Module, data: Data, block_size: int, batch_size: int, device: torch.device
 ) -> dict[str, torch.Tensor]:
     out = {}
     model.eval()
@@ -21,6 +27,7 @@ def estimate_loss(
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
             x, y = data.get_batch(split, block_size=block_size, batch_size=batch_size)
+            x, y = x.to(device), y.to(device)
             _, loss = model(x, y)
             losses[k] = loss.item()
         out[split] = losses.mean()
@@ -30,3 +37,48 @@ def estimate_loss(
 
 def count_parameters(model: torch.nn.Module) -> int:
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+@dataclass
+class TrainingState:
+    training_steps: int
+    training_time: int
+    model_config: GPTConfig
+    model: GPTModel
+    optimizer_state_dict: dict[str, Any]
+
+
+def save_training_state(folder: str, training_state: TrainingState) -> None:
+    torch.save(
+        {
+            'model_config': training_state.model_config,
+            'model_state_dict': training_state.model.state_dict(),
+            'optimizer_state_dict': training_state.optimizer_state_dict,
+            'training_steps': training_state.training_steps,
+            'training_time': training_state.training_time,
+        },
+        os.path.join(folder, f'gpt_{training_state.training_steps}.pth'),
+    )
+
+
+def load_training_state(folder: str) -> TrainingState:
+    if not os.path.isdir(folder):
+        raise ValueError(f"Folder {folder} does not exist.")
+
+    checkpoint_files = glob.glob(os.path.join(folder, 'gpt_*.pth'))
+    if not checkpoint_files:
+        raise OSError("No 'gpt_*.pth' files found.")
+
+    latest_checkpoint = max(checkpoint_files, key=os.path.getmtime)
+    checkpoint = torch.load(latest_checkpoint)
+
+    model = GPTModel(checkpoint['model_config'])
+    model.load_state_dict(checkpoint['model_state_dict'])
+
+    return TrainingState(
+        training_steps=checkpoint['training_steps'],
+        training_time=checkpoint['training_time'],
+        model_config=checkpoint['model_config'],
+        model=model,
+        optimizer_state_dict=checkpoint['optimizer_state_dict'],
+    )
--- main/train.py
+++ main/train.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from dataclasses import dataclass
 from time import time
 
@@ -6,7 +7,14 @@ import torch
 
 from src.data import Data, Vocabulary
 from src.gpt import GPTConfig, GPTModel
-from src.utils import count_parameters, estimate_loss, get_tinyshakespeare_dataset
+from src.utils import (
+    count_parameters,
+    estimate_loss,
+    get_tinyshakespeare_dataset,
+    load_training_state,
+    save_training_state,
+    TrainingState,
+)
 
 # Create logger
 logging.basicConfig(level=logging.INFO)
@@ -15,80 +23,123 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class TrainingConfig:
-    learning_rate: float
     batch_size: int
     max_iters: int
     eval_interval: int
     eval_iters: int
 
 
-def process_training(data: Data, model: GPTModel, optimizer: torch.optim.Optimizer, config: TrainingConfig) -> None:
+def process_training(
+    data: Data,
+    training_state: TrainingState,
+    config: TrainingConfig,
+    optimizer: torch.optim.Optimizer,
+    device: torch.device,
+) -> TrainingState:
     timer = time()
-    for steps in range(config.max_iters):
-        xb, yb = data.get_batch('train', block_size=model.block_size, batch_size=config.batch_size)
+    model = training_state.model
+    steps = 0
+    for steps in range(training_state.training_steps + 1, training_state.training_steps + 1 + config.max_iters):
+        xb, yb = data.get_batch(
+            'train', block_size=training_state.model_config.block_size, batch_size=config.batch_size
+        )
+        xb, yb = xb.to(device), yb.to(device)
         _, loss = model(xb, yb)
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
         optimizer.step()
-        if steps % eval_interval == 0:
+        if steps % config.eval_interval == 0:
             losses = estimate_loss(
                 eval_iters=config.eval_iters,
                 model=model,
                 data=data,
-                block_size=model.block_size,
+                block_size=training_state.model_config.block_size,
                 batch_size=config.batch_size,
+                device=device,
             )
             logger.info(
                 "step: %d; training time: %d sec; train loss: %.4f; val loss: %.4f",
-                steps + 1,
+                steps,
                 round(time() - timer),
                 losses['train'],
                 losses['val'],
             )
 
+    training_time = round(time() - timer)
+    logger.info("Totla training time %d sec", training_time)
+    result_training_state = TrainingState(
+        model_config=training_state.model_config,
+        model=model,
+        optimizer_state_dict=optimizer.state_dict(),
+        training_time=training_time,
+        training_steps=steps,
+    )
+    return result_training_state
+
+
+def initialize_training_state(new: bool, folder: str | None = None) -> TrainingState:
+    if new:
+        model_config = GPTConfig(
+            vocab_size=vocab.size,
+            block_size=8,
+            emb_size=32,
+            num_heads=4,
+            num_layers=3,
+            dropout=0.2,
+        )
+        model = GPTModel(config=model_config).to(device)
+
+        learning_rate = 1e-3
+        optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+        return TrainingState(
+            model_config=model_config,
+            model=model,
+            optimizer_state_dict=optimizer.state_dict(),
+            training_time=0,
+            training_steps=0,
+        )
+    if folder is None:
+        raise ValueError("folder must be specified when new is False")
+    return load_training_state(folder=folder)
+
 
 if __name__ == '__main__':
     torch.manual_seed(42)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else torch.device('cpu'))
+    logger.info('device: %s', device)
+
+    new_training = False
+    weights_folder = './weights'
 
     text = get_tinyshakespeare_dataset()
 
     vocab = Vocabulary(text=text)
     data = Data(vocab.encode(text))
 
-    block_size = 8
-    emb_size = 32
-    num_heads = 4
-    num_layers = 3
-    dropout = 0.2
+    training_state = initialize_training_state(new=new_training, folder=weights_folder)
+    optimizer = torch.optim.AdamW(training_state.model.parameters())
+    optimizer.load_state_dict(training_state.optimizer_state_dict)
 
-    model_config = GPTConfig(
-        vocab_size=vocab.size,
-        block_size=block_size,
-        emb_size=emb_size,
-        num_heads=num_heads,
-        num_layers=num_layers,
-        dropout=dropout,
-    )
-    model = GPTModel(config=model_config)
-    logger.info('models has %d parameters', count_parameters(model))
-
-    learning_rate = 1e-3
-    batch_size = 32
-    max_iters = 10001
-    eval_interval = 1000
-    eval_iters = 200
+    logger.info('models has %d parameters', count_parameters(training_state.model))
 
     training_config = TrainingConfig(
-        learning_rate=learning_rate,
-        batch_size=batch_size,
-        max_iters=max_iters,
-        eval_interval=eval_interval,
-        eval_iters=eval_iters,
+        batch_size=32,
+        max_iters=10000,
+        eval_interval=1000,
+        eval_iters=200,
     )
+    training_state = process_training(
+        data=data, training_state=training_state, optimizer=optimizer, config=training_config, device=device
+    )
+    save_training_state(weights_folder, training_state)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-    process_training(data, model, optimizer, training_config)
-    logger.info(
-        vocab.decode(model.generate(start_tokens=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[0].tolist())
+    sample_generation = vocab.decode(
+        training_state.model.generate(start_tokens=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[
+            0
+        ].tolist()
     )
+    with open(
+        os.path.join(weights_folder, f'gpt_{training_state.training_steps}_sample_output.txt'), 'w', encoding='utf-8'
+    ) as f:
+        f.write(sample_generation)
